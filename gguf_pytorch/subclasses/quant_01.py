971ba988132a9c530a3b5f352e81ae157b0c9f50
Thien Tran <gau.nernst@yahoo.com.sg>
2025-03-12 23:36:18 +0800

    use GGUF layout

--- gguf_pytorch/subclasses/quant_01.py
+++ gguf_pytorch/subclasses/quant_01.py
@@ -7,78 +7,67 @@ from torch import Tensor
 from ..constants import GGML_TYPE
 
 # https://github.com/ggml-org/llama.cpp/blob/master/ggml/src/ggml-common.h
+BLOCK_SIZE = 32
+LAYOUT_MAP = {
+    GGML_TYPE.Q8_0: [2, BLOCK_SIZE],
+    GGML_TYPE.Q4_0: [2, BLOCK_SIZE // 2],
+    GGML_TYPE.Q4_1: [2, 2, BLOCK_SIZE // 2],
+}
 
 
-def _dequantize(int_data: Tensor, scales: Tensor, offsets: Tensor | None, ggml_type: GGML_TYPE):
+def _dequantize(buffer: Tensor, ggml_type: GGML_TYPE) -> Tensor:
+    layout = LAYOUT_MAP[ggml_type]
+    shape = list(buffer.shape)
+    shape[-1] = shape[-1] // sum(layout) * BLOCK_SIZE
+
+    buffer = buffer.reshape(-1, sum(layout))
+
     if ggml_type == GGML_TYPE.Q8_0:
-        out = int_data.view(torch.int8).float().reshape(-1, Quant01.BLOCK_SIZE) * scales.reshape(-1, 1)
+        scales, int_data = buffer.split(layout, dim=1)
+        out = int_data.view(torch.int8).float() * scales.view(torch.float16)
 
     elif ggml_type == GGML_TYPE.Q4_0:
+        scales, int_data = buffer.split(layout, dim=1)
         data = torch.stack([int_data & 0xF, int_data >> 4], dim=-2).float()
-        out = (data - 8).reshape(-1, Quant01.BLOCK_SIZE) * scales.reshape(-1, 1)
+        out = (data - 8).reshape(-1, BLOCK_SIZE) * scales.view(torch.float16)
 
     elif ggml_type == GGML_TYPE.Q4_1:
+        scales, offsets, int_data = buffer.split(layout, dim=1)
         data = torch.stack([int_data & 0xF, int_data >> 4], dim=-2).float()
-        out = data.reshape(-1, Quant01.BLOCK_SIZE) * scales.reshape(-1, 1) + offsets.reshape(-1, 1)
+        out = data.reshape(-1, BLOCK_SIZE) * scales.view(torch.float16) + offsets.view(torch.float16)
 
-    return out.to(scales.dtype)
+    return out.to(torch.float16).view(shape)
 
 
-def _reshape(x: "Quant01"):
-    shape = list(x.shape)
-    if x.ggml_type in (GGML_TYPE.Q4_0, GGML_TYPE.Q4_1):
-        shape[-1] //= 2
-    int_data = x.int_data.flatten().view(shape)
-
-    shape = x.shape[:-1] + (shape[-1] // x.BLOCK_SIZE,)
-    scales = x.scales.flatten().view(shape)
-    offsets = x.offsets.flatten().view(shape) if x.offsets is not None else None
-
-    return int_data, scales, offsets
+def _reshape(buffer: Tensor, ggml_type: GGML_TYPE, shape: tuple[int, ...]) -> Tensor:
+    assert shape[-1] % BLOCK_SIZE == 0
+    nbytes_per_block = sum(LAYOUT_MAP[ggml_type])
+    buffer_shape = list(shape[:-1]) + [shape[-1] // BLOCK_SIZE * nbytes_per_block]
+    return buffer.reshape(buffer_shape)
 
 
 class Quant01(Tensor):
-    BLOCK_SIZE = 32
-
     @staticmethod
-    # @torch._dynamo.disable
-    def __new__(
-        cls,
-        int_data: Tensor,
-        scales: Tensor,
-        offsets: Tensor | None,
-        ggml_type: GGML_TYPE,
-        shape: tuple[int, ...],
-    ):
+    def __new__(cls, buffer: Tensor, ggml_type: GGML_TYPE):
+        shape = list(buffer.shape)
+        nbytes_per_block = sum(LAYOUT_MAP[ggml_type])
+        shape[-1] = shape[-1] // nbytes_per_block * BLOCK_SIZE
         return Tensor._make_wrapper_subclass(
             cls,
             shape,
-            dtype=scales.dtype,
-            device=int_data.device,
+            dtype=torch.float16,
+            device=buffer.device,
         )
 
-    # @torch._dynamo.disable
-    def __init__(
-        self,
-        int_data: Tensor,
-        scales: Tensor,
-        offsets: Tensor | None,
-        ggml_type: GGML_TYPE,
-        shape: tuple[int, ...],
-    ):
-        assert int_data.dtype is torch.uint8
-        self.int_data = int_data
-        self.scales = scales
-        self.offsets = offsets
+    def __init__(self, buffer: Tensor, ggml_type: GGML_TYPE) -> None:
+        assert buffer.dtype is torch.uint8
+        self.buffer = buffer
         self.ggml_type = ggml_type
 
         # TODO: add kernel backend e.g. tinygemm, gemlite. then, preprocess the weights to the one required by that backend.
 
     def __tensor_flatten__(self):
-        if self.offsets is not None:
-            return ["int_data", "scales", "offsets"], [self.ggml_type, self.shape]
-        else:
-            return ["int_data", "scales"], [self.offsets, self.ggml_type, self.shape]
+        return ["buffer"], [self.ggml_type]
 
     @classmethod
     def __tensor_unflatten__(cls, tensor_data_dict, tensor_attributes, outer_size=None, outer_stride=None):
@@ -88,27 +77,13 @@ class Quant01(Tensor):
     def from_buffer(buffer: Tensor, ggml_type: GGML_TYPE, shape: tuple[int, ...]):
         assert buffer.dtype == torch.uint8
 
-        layout = {
-            GGML_TYPE.Q8_0: [2, Quant01.BLOCK_SIZE],
-            GGML_TYPE.Q4_0: [2, Quant01.BLOCK_SIZE // 2],
-            GGML_TYPE.Q4_1: [2, 2, Quant01.BLOCK_SIZE // 2],
-        }[ggml_type]
-
-        nbytes = math.prod(shape) // Quant01.BLOCK_SIZE * sum(layout)
-        buffer = buffer[:nbytes].view(-1, sum(layout))
-
-        if len(layout) == 2:
-            scales, int_data = buffer.split(layout, dim=1)
-            offsets = None
-        else:
-            scales, offsets, int_data = buffer.split(layout, dim=1)
-            offsets = offsets.view(torch.float16)
-
-        scales = scales.view(torch.float16)
-        return Quant01(int_data, scales, offsets, ggml_type, shape)
+        nbytes_per_block = sum(LAYOUT_MAP[ggml_type])
+        nbytes = math.prod(shape) // BLOCK_SIZE * nbytes_per_block
+        buffer = _reshape(buffer[:nbytes], ggml_type, shape)
+        return Quant01(buffer, ggml_type)
 
     def dequantize(self):
-        return _dequantize(self.int_data, self.scales, self.offsets, self.ggml_type).view(self.shape)
+        return _dequantize(self.buffer, self.ggml_type)
 
     def __repr__(self):
         fields = dict(
@@ -132,14 +107,7 @@ class Quant01(Tensor):
         elif func is F.embedding:
             input: Tensor = args[0]
             weight: Quant01 = args[1]
-            int_data, scales, offsets = _reshape(weight)
-            out = _dequantize(
-                F.embedding(input, int_data),
-                F.embedding(input, scales),
-                F.embedding(input, offsets) if offsets is not None else None,
-                weight.ggml_type,
-            )
-            return out.view(input.shape + weight.shape[1:])
+            return _dequantize(F.embedding(input, weight.buffer), weight.ggml_type)
 
         with torch._C.DisableTorchFunctionSubclass():
             return func(*args, **kwargs)
@@ -160,53 +128,38 @@ class Quant01(Tensor):
             else:
                 assert math.prod(shape) == x.numel()
 
-            assert shape[-1] % Quant01.BLOCK_SIZE == 0
-            return Quant01(x.int_data, x.scales, x.offsets, x.ggml_type, tuple(shape))
+            return Quant01(_reshape(x.buffer, x.ggml_type, shape), x.ggml_type)
 
         elif func is aten.transpose.int:
             x: Quant01 = args[0]
             dim0 = args[1]
             dim1 = args[2]
 
-            int_data, scales, offsets = _reshape(x)
-            int_data = int_data.transpose(dim0, dim1)
-            scales = scales.transpose(dim0, dim1)
-            offsets = offsets.transpose(dim0, dim1) if offsets is not None else None
-
-            shape = list(x.shape)
-            shape[dim0], shape[dim1] = shape[dim1], shape[dim0]
-            return Quant01(int_data, scales, offsets, x.ggml_type, shape)
+            # if transpose involves the last dimension, we have to requantize
+            assert dim0 not in (-1, x.ndim - 1)
+            assert dim1 not in (-1, x.ndim - 1)
+            return Quant01(x.buffer.transpose(dim0, dim1), x.ggml_type)
 
         elif func is aten.cat.default:
-            tensors = args[0]
+            tensors: list[Quant01] = args[0]
 
             assert all(isinstance(x, Quant01) for x in tensors)
             assert all(x.ggml_type == tensors[0].ggml_type for x in tensors)
+            ggml_type = tensors[0].ggml_type
 
-            int_data_list, scales_list, offsets_list = zip(*[_reshape(x) for x in tensors])
-            int_data = func(int_data_list, *args[1:])
-            scales = func(scales_list, *args[1:])
-            offsets = func(offsets_list, *args[1:]) if offsets_list[0] is not None else None
-
-            shape = list(scales.shape)
-            shape[-1] *= Quant01.BLOCK_SIZE
-            return Quant01(int_data, scales, offsets, tensors[0].ggml_type, shape)
+            buffer_list = [_reshape(x.buffer, ggml_type, x.shape) for x in tensors]
+            buffer = func(buffer_list, *args[1:])
+            return Quant01(buffer, ggml_type)
 
         elif func is aten.detach.default:
             x: Quant01 = args[0]
-            return Quant01(x.int_data, x.scales, x.offsets, x.ggml_type, x.shape)
+            return Quant01(x.buffer, x.ggml_type)
 
         elif func is aten._to_copy.default:
             x: Quant01 = args[0]
-            dtype = kwargs.get("dtype", None)
+            # NOTE: we don't support changing dtype
             device = kwargs.get("device", None)
-            return Quant01(
-                x.int_data.to(device=device),
-                x.scales.to(device=device, dtype=dtype),
-                x.offsets.to(device=device, dtype=dtype) if x.offsets is not None else None,
-                x.ggml_type,
-                x.shape,
-            )
+            return Quant01(x.buffer.to(device=device), x.ggml_type)
 
         msg = f"{cls.__name__} dispatch: {func} is not implemented"
         for i, arg in enumerate(args):
